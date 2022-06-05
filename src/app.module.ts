70a3a28346b85eeea24c16859baa46b5438ba192
JackDrakkar <jack.d.drakkar@gmail.com>
2022-06-05 15:53:37 +0300

    remove forced ssl config from database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
-import { join } from 'path';
-import { readFileSync } from 'fs';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import appConfig, { IAppConfig, APP_NAMESPACE } from './configuration/app/app.config';
-import dbConfig, { IMongoDbConfig, MONGODB_NAMESPACE } from './configuration/mongodb/mongodb.config';
+import appConfig from './configuration/app/app.config';
+import mongoDbConfig, { IMongoDbConfig, MONGODB_NAMESPACE } from './configuration/mongodb/mongodb.config';
 import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 import { HealthModule } from './health/health.module';
 import { UsersModule } from './users/users.module';
@@ -12,23 +10,16 @@ import { UsersModule } from './users/users.module';
   imports: [
     ConfigModule.forRoot({
       cache: true,
-      load: [appConfig, dbConfig],
+      load: [appConfig, mongoDbConfig],
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => {
         const mongoDbCfg = configService.get<IMongoDbConfig>(MONGODB_NAMESPACE);
-        const appCfg = configService.get<IAppConfig>(APP_NAMESPACE);
         const dbConfig: MongooseModuleOptions = {
           uri: mongoDbCfg.uri,
           dbName: mongoDbCfg.database,
         };
-        if (appCfg.env === 'production') {
-          // FIXME: AWS DocumentDB connection string uses ssl_ca_certs option, which is not supported (use sslCA option instead?)
-          dbConfig.ssl = true;
-          dbConfig.sslValidate = true;
-          dbConfig.sslCA = readFileSync(join(process.cwd(), 'rds-combined-ca-bundle.pem'), { encoding: 'ascii' });
-        }
         return dbConfig;
       },
       inject: [ConfigService],
